a99690f746bfa91ec6758f5caaed70d0fac790ed
Partizan-coder <79650117+Partizan-coder@users.noreply.github.com>
2022-02-16 01:43:12 +0300

    Update urls.py

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -4,4 +4,7 @@ from measurement.views import SensorsView, SensorView, MeasurementView
 urlpatterns = [
     path('sensors/', SensorsView.as_view()),
     path('sensors/<pk>/', SensorsView.as_view()),
+    path('sensor/<pk>/', SensorView.as_view()),
     path('measurements/', MeasurementView.as_view()),
+
+]
