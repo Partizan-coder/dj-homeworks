2d7187a17368cbc00dc6d177c4ed8748cb07e418
Partizan-coder <79650117+Partizan-coder@users.noreply.github.com>
2022-02-16 01:42:30 +0300

    Update models.py

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -2,15 +2,11 @@ import datetime
 
 from django.db import models
 
-
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
-
 class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
 
-
 class Measurement(models.Model):
-    sensor = models.IntegerField()
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensor')
     temperature = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
