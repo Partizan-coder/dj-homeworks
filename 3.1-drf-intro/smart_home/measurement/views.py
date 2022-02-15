b20a0b5812fbe755e2aa86da23e5a51bd93e1794
Partizan-coder <79650117+Partizan-coder@users.noreply.github.com>
2022-02-16 01:44:05 +0300

    Update views.py

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,7 +1,5 @@
-from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Sensor, Measurement
 from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
@@ -16,13 +14,11 @@ class SensorsView(ListAPIView):
             sensorSer.save()
         return Response({'status': 'OK, data posted successfully'})
 
-    def patch(self, request, pk, 'ep111'):
-        sensor = Sensor.objects.get(pk)
-        requestData = request.data
-        print(requestData)
-        sensorSer = SensorSerializer(sensor, data=request.data)
-        if sensorSer.is_valid():
-            sensorSer.save()
+    def patch(self, request, pk):
+        sensor = self.get_object()
+        sensor.name = request.data.get("name")
+        sensor.description = request.data.get("description")
+        sensor.save()
         return Response({'status': 'OK, data patched successfully'})
 
 
